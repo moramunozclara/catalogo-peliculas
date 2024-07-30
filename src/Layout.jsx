c79027c764df103832b0b0e03ef172147b3fb2c4
Clara Mora <moramunozclara@alumnos.cei.es>
2024-07-30 20:58:18 +0200

    fix

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,20 +1,40 @@
+// LAYOUT CON CREATE CONTEXT
+
+//1º sfc
+// 2º Import el OUTLET y meterlo en el MAIN
+// 3º Import el css
+// 4º importar el create context para el tema
+// 5ª meter todo el return dentro del context
+
 import { Outlet, } from "react-router-dom";
 import './css/Layout.css';
 import './css/catalogo-peliculas.css';
 
+import {createContext, useState} from 'react';
+
+// creación del contexto para el tema
+export const ModoOscuroContext = createContext();
 
 const Layout = () => {
+      const [tema, setTema] = useState("light");
 
+      const toggleTema = () => {
+        setTema((prevTema) => (prevTema === "light" ? "dark" : "light"));
+      };
 
   return ( 
-    <>
-    <header>
-      <h1>Estoy en el inicio</h1>
-    </header>
-      <main>
-          <Outlet/>
-      </main>
-    </>
+
+    <ModoOscuroContext.Provider value={{ tema, toggleTema }}>
+      <>
+        <header>
+          <h1>Estoy en el inicio</h1>
+        </header>
+          <main>
+              <Outlet/>
+          </main>
+      </>
+
+    </ModoOscuroContext.Provider>
    );
 }
  
--- src/pages/CatalogoPeliculas.jsx
+++ src/pages/CatalogoPeliculas.jsx
@@ -1,16 +1,28 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useContext} from 'react';
+import { ModoOscuroContext } from '../Layout' // Importar el contexto
+
 
 
 const CatalogoPeliculas = () => {
 
     const [peliculas, setPeliculas] = useState([]);
+    const [usuarios, setUsuarios] = useState([]);
+
+    // Función para alternar el tema
+    const {tema, toggleTema} = useContext(ModoOscuroContext); // Consumir el contexto
+
 
     useEffect(() => {
-        const getPeliculas = async() => {
+        const getDatos = async() => {
             try {
-                const respuesta = await fetch ('/backend/API/v1/peliculas.json');
-                const objeto = await respuesta.json();
-                setPeliculas(objeto.peliculas);
+                const respuestaPeliculas = await fetch ('/backend/API/v1/peliculas.json');
+                const objetoPeliculas = await respuestaPeliculas.json();
+                setPeliculas(objetoPeliculas.peliculas);
+
+                const respuestaUsuarios = await fetch ('/backend/API/v1/usuarios.json');
+                const objetoUsuarios = await respuestaUsuarios.json();
+                setUsuarios(objetoUsuarios.usuarios);
+
             }
             catch (error) {
                 console.error('Error al obtener las películas', error)
@@ -19,29 +31,61 @@ const CatalogoPeliculas = () => {
 
         
 
-        getPeliculas(); // obtén las películas
+        getDatos(); // obtén las películas
 
         
     }, []);
 
 
+    // Filtros para mostrar según usuario
+    // peliculas de usuario equivale a peliculadas filtradas por su key idUsuario igual a el nombre de usuario
+    const peliculasUser1 = peliculas.filter(pelicula => pelicula.idUsuario === 'user1');
+    const peliculasUser2 = peliculas.filter(pelicula => pelicula.idUsuario === 'user2');
+    
 
     return ( 
-        <>
+        <div className={tema === "dark" ? 'oscuro' : 'claro'}>
+                  <button onClick={toggleTema}>
+                    Cambiar a {tema === "dark" ? 'Tema Claro' : 'Tema Oscuro'}
+                    </button>
+
             <h2>Este es tu catálogo de películas
             {/* https://www.freecodecamp.org/news/how-to-make-an-image-search-app-in-react/ */}
             </h2>
 
 
-
-            <ul className="CatalogoGrid">
+            <div className='ContainerPeliculas'>
+                
+            <h3>Todas las películas</h3>
+            <ul className="CatalogoGrid"> 
                 {peliculas.map((pelicula) => (
-                    <li key={pelicula.id}>{pelicula.titulo}</li>
+                    <li key={pelicula.id}>
+                        <img src={pelicula.imagen} alt={pelicula.titulo}/>
+                        {pelicula.titulo}</li>
                 ))}
             </ul>
+            </div>
+
+            <div className='ContainerPeliculas'>
+            <h3>Películas de Clara (user1)</h3>
+            <ul className="CatalogoGrid"> 
+                {peliculasUser1.map((pelicula) => (
+                    <li key={pelicula.id}><img src={pelicula.imagen} alt={pelicula.titulo}/>{pelicula.titulo}</li>
+                ))}
+            </ul>
+            </div>
+
+            <div className='ContainerPeliculas'>
+            <h3>Películas de Roberto (user2)</h3>
+            <ul className="CatalogoGrid"> 
+                {peliculasUser2.map((pelicula) => (
+                    <li key={pelicula.id}><img src={pelicula.imagen} alt={pelicula.titulo}/>{pelicula.titulo}</li>
+                ))}
+            </ul>
+            </div>
 
 
-        </>
+        </div>
      );
 }
  
